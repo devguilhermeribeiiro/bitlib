8cae45a54489a543d57cecbcc4a141df7a5cd11f
guilherme <devguilhermeribeiro000@gmail.com>
2025-02-18 23:22:11 -0300

    Setup nginx to serve static files

--- cache/views/2b58ce4299f0dbdf299777dec38c6634.php
+++ cache/views/2b58ce4299f0dbdf299777dec38c6634.php
@@ -4,32 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo e($title); ?></title>
-    <style>
-        .file-input {
-            display: none; /* Esconde o input real */
-        }
-
-        .file-label {
-            display: inline-block;
-            background-color: #4CAF50; /* Cor de fundo */
-            color: white; /* Cor do texto */
-            padding: 10px 20px; /* Espaçamento interno */
-            cursor: pointer; /* Mostra um cursor de ponteiro */
-            border-radius: 5px; /* Bordas arredondadas */
-            font-size: 16px; /* Tamanho da fonte */
-        }
-
-        .file-label:hover {
-            background-color: #45a049; /* Cor do fundo ao passar o mouse */
-        }
-
-        /* Exibe o nome do arquivo selecionado */
-        .file-name {
-            margin-top: 10px;
-            font-size: 14px;
-            color: #333;
-        }
-    </style>
+    <link rel="stylesheet" href="/css/style.css">
 
 </head>
 <body>
--- views/home.blade.php
+++ views/home.blade.php
@@ -4,14 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
+    <link rel="stylesheet" href="/css/style.css">
+
 </head>
 <body>
-    <h1>{{ $message }}</h1>
+    <input type="file" name="file" id="file" class="file-input">
+    <label for="file" class="file-label">Escolher arquivo</label>
+    <p class="file-name" id="file-name">Nenhum arquivo selecionado</p>
+    <button id="openModal">Abrir modal</button>
+    <dialog id="dialog">
+        <h1>OI</h1>
+        <button id="closeModal">Fechar modal</button>
+    </dialog>
+    <script>
+    // Exibe o nome do arquivo selecionado
+    document.getElementById('file').addEventListener('change', function() {
+        const fileName = document.getElementById('file-name');
+        fileName.textContent = this.files.length > 0 ? this.files[0].name : 'Nenhum arquivo selecionado';
+    });
+    
+    const dialog = document.getElementById("dialog");
+    const openModal = document.getElementById("openModal");
+    const closeModal = document.getElementById("closeModal");
 
-    @if(isset($user))
-        <p>Usuário logado: {{ $user['name'] }}</p>
-    @else
-        <p>Nenhum usuário logado.</p>
-    @endif
+    openModal.addEventListener('click', () => {
+        dialog.showModal();
+    });
+
+    closeModal.addEventListener('click', () => {
+        dialog.close();
+    });
+    </script>
 </body>
 </html>
